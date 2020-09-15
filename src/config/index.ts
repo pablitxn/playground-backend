1d6edbeb3fd7bcfc400d31831b2890222b672d5d
Pablo Coronel <me@pablitxn.io>
2020-09-15 00:45:24 -0300

    hotfix(dotenv)

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,13 @@
 // Dotenv
 import dotenv from 'dotenv'
 
-const envFound = dotenv.config()
-if (envFound.error) {
-	// This error should crash whole process
-	throw new Error("⚠️  Couldn't find .env file  ⚠️")
+// Set the NODE_ENV to 'development' by default
+if (process.env.NODE_ENV === 'development') {
+	const envFound = dotenv.config()
+	if (envFound.error) {
+		// This error should crash whole process
+		throw new Error("⚠️  Couldn't find .env file  ⚠️")
+	}
 }
 
 export default {
