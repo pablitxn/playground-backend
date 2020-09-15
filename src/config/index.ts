c73ac559303b2ccb7766ed40a15bd5746d814bae
Pablo Coronel <me@pablitxn.io>
2020-09-15 00:33:04 -0300

    hotfix(dotenv)

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,6 @@
 // Dotenv
 import dotenv from 'dotenv'
 
-// Set the NODE_ENV to 'development' by default
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
 const envFound = dotenv.config()
 if (envFound.error) {
 	// This error should crash whole process
